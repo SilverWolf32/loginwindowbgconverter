57f62203da30c9289f20c6dd158bd6a4c3b095da
SilverWolf <43831298+SilverWolf32@users.noreply.github.com>
2018-05-15 18:30:35 -0400

    Implement image loading

--- loginwindowbgconverter.swift
+++ loginwindowbgconverter.swift
@@ -1,2 +1,19 @@
-print("Hello, world!")
+import Foundation
+import AppKit.NSImage
 
+func printUsage() {
+	print("usage: \(CommandLine.arguments[0]) \u{1B}[4mimage-file\u{1B}[0m")
+}
+
+guard CommandLine.arguments.indices.contains(1) else {
+	printUsage()
+	exit(1)
+}
+let inputFile = CommandLine.arguments[1]
+
+// print("Filename: \(inputFile)")
+
+guard let image = NSImage(contentsOfFile: inputFile) else {
+	print("\(CommandLine.arguments[0]): cannot load image from \(inputFile)")
+	exit(2)
+}
