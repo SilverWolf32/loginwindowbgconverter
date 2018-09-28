06f8efd676ed6dc5a641c26610f28a4934efedd8
SilverWolf <43831298+SilverWolf32@users.noreply.github.com>
2018-09-28 00:14:35 -0400

    Add license -- public domain
    Because why not?
    Also fix a typo in final save error handling, and make the wording more consistent.

--- loginwindowbgconverter.swift
+++ loginwindowbgconverter.swift
@@ -1,3 +1,34 @@
+//
+// loginwindowbgconverter
+// by SilverWolf
+// 2018-09-27
+// 
+// This is free and unencumbered software released into the public domain.
+//
+// Anyone is free to copy, modify, publish, use, compile, sell, or
+// distribute this software, either in source code form or as a compiled
+// binary, for any purpose, commercial or non-commercial, and by any
+// means.
+//
+// In jurisdictions that recognize copyright laws, the author or authors
+// of this software dedicate any and all copyright interest in the
+// software to the public domain. We make this dedication for the benefit
+// of the public at large and to the detriment of our heirs and
+// successors. We intend this dedication to be an overt act of
+// relinquishment in perpetuity of all present and future rights to this
+// software under copyright law.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+// OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+//
+// For more information, please refer to <http://unlicense.org/>
+//
+
 import Foundation
 // import AppKit.NSImage
 // import AppKit.NSScreen
@@ -147,7 +178,7 @@ if #available(macOS 10.14, *) { // macOS Mojave has a completely different syste
 		try imageData.write(to: URL(fileURLWithPath: targetFile))
 	} catch {
 		print("\(CommandLine.arguments[0]): can't write image data: \(error)")
-		print("(are yuou root?)")
+		print("(are you root?)")
 		exit(1)
 	}
 } else {
@@ -159,7 +190,7 @@ if #available(macOS 10.14, *) { // macOS Mojave has a completely different syste
 		try pngData.write(to: URL(fileURLWithPath: targetFile))
 	} catch {
 		print("\(CommandLine.arguments[0]): can't write image data: \(error)")
-		print("(do you have write permission?)")
+		print("(are you root?)")
 		exit(1)
 	}
 }
